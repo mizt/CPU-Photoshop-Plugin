0f3519685571cb735355dbeeef2d99e7685d04b4
mizt <info@mizt.org>
2019-02-21 09:11:01 +0900

    updated

--- bin/CPU.plugin/Contents/Resources/build.sh
+++ bin/CPU.plugin/Contents/Resources/build.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
-dir=$(cd $(dirname $0)&&pwd)
-cd "$dir"
+#!/bin/bash
+cd "$(dirname "$0")"
 clang++ ./main.mm -dynamiclib -std=c++14 -Wc++14-extensions -arch x86_64 -fobjc-arc -framework Cocoa -O3 -o ./CPU.dylib
